c2985d7dcd2499ce32a7465b4a57fb0ba0521c98
Ruth Santana Silva <rutysantana.s@gmail.com>
2023-08-18 11:00:31 -0300

    exercicios

---
+++ cursojs/aula12/ex010.js
@@ -0,0 +1,9 @@
+var idade = 69
+
+if (idade < 16) {
+    console.log('não vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opicional')
+} else {
+    console.log('Voto obrigatório')
+}
