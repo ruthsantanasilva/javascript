56ebd8c70ee9ce8ab72e39389979cfc8637b423c
Ruth Santana Silva <rutysantana.s@gmail.com>
2023-08-18 19:16:12 -0300

    ambiente.js

---
+++ cursojs/aula13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+
+/*var c = 1
+while (c <= 6){
+    console.log ('Tudo bem?')
+    c++
+}*/
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
