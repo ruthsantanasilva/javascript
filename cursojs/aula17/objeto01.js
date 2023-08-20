52b91a7d98184632b74656b7f45d581ba10716ce
Ruth Santana Silva <rutysantana.s@gmail.com>
2023-08-20 16:10:22 -0300

    up

--- cursojs/aula17/objeto01.js
+++ cursojs/aula17/objeto01.js
@@ -3,10 +3,10 @@ let amigo = {
   sexo: "M",
   peso: 85.4,
   engordar(p = 0) {
-    console.log("Engordou");
+    console.log("Engordou")
     this.peso += p;
   }
 }
 
-amigo.engordar(20);
-console.log(`${amigo.nome} pesa ${amigo.peso} Kg`);
+amigo.engordar(20)
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)
